99e4979590dcbdf0882db853b3f7acf38e6a1fb6
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-05-01 00:25:49 +0530

    Add project files.

---
+++ MiddlewareExample/CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,21 @@
+﻿namespace MiddlewareExample.CustomMiddleware
+{
+    public class CustomMiddlewareExtensionCoreMVC6 : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("My Custom Middleware - Starts\n");
+            await next(context);
+            await context.Response.WriteAsync("My Custom Middleware - Ends\n");
+        }
+    }
+
+    public static class CustomMiddlewareExtension
+    {
+        public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<CustomMiddlewareExtensionCoreMVC6>();
+        }
+    }
+
+}
